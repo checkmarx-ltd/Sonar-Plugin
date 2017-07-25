b012c25f34f83a33bf2bbcd5fa752c020744a525
cxZoharby <Zohar.BenYesha@checkmarx.com>
2017-07-25 21:25:49 +0300

    C# and vb.net rules   by: zoharby

--- src/main/java/com/checkmarx/sonar/cxrules/CXProgrammingLanguage.java
+++ src/main/java/com/checkmarx/sonar/cxrules/CXProgrammingLanguage.java
@@ -7,7 +7,7 @@ package com.checkmarx.sonar.cxrules;
 public enum CXProgrammingLanguage {
     CPP("CPP","cpp",4), GO("GO","go",131072), GROOVY("Groovy","grvy",32768), JAVA("Java","java",2), JAVASCRIPT("JavaScript","js",8),
     OBJECTIVE_C("Objc","objc",4096), PERL("Perl","perl",2048), PHP("PHP","php",512), PLSQL("PLSQL","plsql",8192), PYTHON("Python","py",16384),
-    RUBY("Ruby","ruby",1024), SCALA("Scala","scala",65536),VB6("VB6","vb6",256);
+    RUBY("Ruby","ruby",1024), SCALA("Scala","scala",65536),VB6("VB6","vb6",256), CSHARP("csharp","cs",1), VBNET("vbnet","vbnet",32);
 
     private String name;
     private String sonarName;
