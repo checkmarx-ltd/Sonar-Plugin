5a3c740e3b01cc220e5f2e965f5aa42ef739bc91
ilandn <ilandayans@gmail.com>
2021-11-22 12:07:32 -0600

    bugid: Ensure delete legacy property CR_by: n/a

--- src/main/java/com/checkmarx/sonar/sensor/execution/SastResultsCollector.java
+++ src/main/java/com/checkmarx/sonar/sensor/execution/SastResultsCollector.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.sonar.api.batch.fs.FileSystem;
 import org.sonar.api.batch.fs.InputFile;
+import org.sonar.api.batch.fs.internal.DefaultInputFile;
 import org.sonar.api.batch.rule.ActiveRule;
 import org.sonar.api.batch.rule.ActiveRules;
 import org.sonar.api.batch.sensor.SensorContext;
@@ -62,7 +63,7 @@ public class SastResultsCollector {
 
                     ActiveRule rule = findRuleAndHandleErrors(activeRules, result.getQuery());
                     SastSeverity sastSeverity = getSastSeverity(result);
-                    if(!checkValidity(result,rule,sastSeverity)){
+                    if (!checkValidity(result, rule, sastSeverity)) {
                         continue;
                     }
 
@@ -85,7 +86,7 @@ public class SastResultsCollector {
         }//files loop
     }
 
-    private boolean checkValidity(CxResultToSonarResult result,ActiveRule rule,SastSeverity sastSeverity){
+    private boolean checkValidity(CxResultToSonarResult result, ActiveRule rule, SastSeverity sastSeverity) {
         if ("1".equals(result.getResultData().getState())) {
             //continue if result state is "Not Exploitable"
             return false;
@@ -102,181 +103,183 @@ public class SastResultsCollector {
         return true;
     }
 
-    private void init(SensorContext context){
+    private void init(SensorContext context) {
         mainFiles = getMainFiles(context);
         activeRules = context.activeRules();
         setRemediationEffortPerVulnerability(context);
     }
 
 
-    private Iterable<InputFile> getMainFiles(SensorContext context){
+    private Iterable<InputFile> getMainFiles(SensorContext context) {
         FileSystem fs = context.fileSystem();
-        if(fs == null){
+        if (fs == null) {
             logger.error("File system was not provided.");
             return new ArrayList<>();
         }
 
         Iterable<InputFile> mainFiles = fs.inputFiles(fs.predicates().hasType(InputFile.Type.MAIN));
-        if(mainFiles == null){
+        if (mainFiles == null) {
             logger.info("File system has no Main folder.");
             return new ArrayList<>();
         }
         return mainFiles;
     }
 
-        private SastSeverity getSastSeverity(CxResultToSonarResult result){
-            SastSeverity sastSeverity = SastSeverity.fromName(result.getResultData().getSeverity());
-            if(sastSeverity == null){
-                int index = result.getQuery().getSeverityIndex() != null ? Integer.valueOf(result.getQuery().getSeverityIndex()):null;
-                sastSeverity = SastSeverity.fromId(index);
-            }
-            return sastSeverity;
+    private SastSeverity getSastSeverity(CxResultToSonarResult result) {
+        SastSeverity sastSeverity = SastSeverity.fromName(result.getResultData().getSeverity());
+        if (sastSeverity == null) {
+            int index = result.getQuery().getSeverityIndex() != null ? Integer.valueOf(result.getQuery().getSeverityIndex()) : null;
+            sastSeverity = SastSeverity.fromId(index);
         }
+        return sastSeverity;
+    }
 
 
-        private void setRemediationEffortPerVulnerability(SensorContext context){
-            String remediationEffortInSonarDb = context.settings().getString(CxProperties.CX_REMEDIATION_EFFORT);
-            if((remediationEffortInSonarDb != null) && !remediationEffortInSonarDb.equals("0")){
-                try {
-                    remediationEffortPerVulnerability = Double.valueOf(remediationEffortInSonarDb);
-                }catch (Exception ignored)
-                {}
-            }
-        }
-
-
-        private ActiveRule findRuleAndHandleErrors(ActiveRules activeRules, CxXMLResults.Query query){
-            CXProgrammingLanguage language = CXProgrammingLanguage.fromLanguageName(query.getLanguage());
-            if(language == null){
-                logger.error("Unknown language: " + query.getLanguage() + "for query: " + query.getName());
-                return null;
-            }
-
-            Collection<ActiveRule> rules = activeRules.findByRepository(language.getSonarRuleRepository());
-            ActiveRule rule = null;
-            for(ActiveRule currRule : rules) {
-                if (currRule.ruleKey().rule().equals("checkmarx_" + query.getId())) {
-                    rule = currRule;
-                    break;
-                }
-            }
-
-            if(rule == null){
-                logger.info("Rule: " + "checkmarx_" + query.getId() + " is not active or not existing. It will not appear in Checkmarx scan results.");
-                logger.info("If rule exists in " + language.getSonarRuleRepository() + " rule repository, you can update it to your quality profile.");
-            }
-
-            return rule;
-        }
-
-        private void updateQueryToCurrFile(CxResultToSonarResult nonIssueResult){
-            SastSeverity severity = SastSeverity.fromName(nonIssueResult.getResultData().getSeverity());
-            if(severity == null){
-                logger.error("Result for query " + nonIssueResult.getQuery().getName() + " has no severity. Checkmarx result may be incomplete.");
-                return;
-            }
-            switch (severity) {
-                    case SAST_HIGH:
-                        currFileQueriesCollector.addHighQuery(nonIssueResult.getQuery().getName());
-                        break;
-                    case SAST_MEDIUM:
-                        currFileQueriesCollector.addMediumQuery(nonIssueResult.getQuery().getName());
-                        break;
-                    case SAST_LOW:
-                        currFileQueriesCollector.addLowQuery(nonIssueResult.getQuery().getName());
-                        break;
-                default:
-                    break;
-                }
-        }
-
-        private void updateCurrFileVulnerabilities(CxResultToSonarResult result){
-            SastSeverity severity = SastSeverity.fromName(result.getResultData().getSeverity());
-            if(severity == null){
-                logger.error("Result for query " + result.getQuery().getName() + " has no severity. Checkmarx result may be incomplete.");
-                return;
-            }
-            boolean isNew = "New".equals(result.getResultData().getStatus());
-
-            switch (severity) {
-                case SAST_HIGH:
-                    currFileSumVulnerabilityCounter.incrementHigh();
-                    if (isNew) {
-                        currFileNewVulnerabilityCounter.incrementHigh();
-                    }
-                    break;
-                case SAST_MEDIUM:
-                    currFileSumVulnerabilityCounter.incrementMedium();
-                    if (isNew) {
-                        currFileNewVulnerabilityCounter.incrementMedium();
-                    }
-                    break;
-                case SAST_LOW:
-                    currFileSumVulnerabilityCounter.incrementLow();
-                    if (isNew) {
-                        currFileNewVulnerabilityCounter.incrementLow();
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        private void saveCxCustomMetrics(SensorContext context, InputFile file){
+    private void setRemediationEffortPerVulnerability(SensorContext context) {
+        String remediationEffortInSonarDb = context.settings().getString(CxProperties.CX_REMEDIATION_EFFORT);
+        if ((remediationEffortInSonarDb != null) && !remediationEffortInSonarDb.equals("0")) {
             try {
-                addSumVulnerabilitiesMetrics(context, file);
-                addNewVulnerabilitiesMetrics(context, file);
-            } catch (Exception e) {
-                String errMsg = "Error saving Checkmarx vulnerabilities to to file: " + file.absolutePath() + "\nError: " + e.getMessage() +
-                        "\nPresented scan measures and report might be incomplete.";
-                logger.error(errMsg);
-                context.newAnalysisError().onFile(file).message(errMsg);
+                remediationEffortPerVulnerability = Double.valueOf(remediationEffortInSonarDb);
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
+
+    private ActiveRule findRuleAndHandleErrors(ActiveRules activeRules, CxXMLResults.Query query) {
+        CXProgrammingLanguage language = CXProgrammingLanguage.fromLanguageName(query.getLanguage());
+        if (language == null) {
+            logger.error("Unknown language: " + query.getLanguage() + "for query: " + query.getName());
+            return null;
+        }
+
+        Collection<ActiveRule> rules = activeRules.findByRepository(language.getSonarRuleRepository());
+        ActiveRule rule = null;
+        for (ActiveRule currRule : rules) {
+            if (currRule.ruleKey().rule().equals("checkmarx_" + query.getId())) {
+                rule = currRule;
+                break;
             }
         }
 
-        private void addSumVulnerabilitiesMetrics(SensorContext context, InputFile file){
-           if(currFileSumVulnerabilityCounter.getSumVulnerabilities() > 0) {
-               addMetric(context, file, SAST_TOTAL_VULNERABILITIES, currFileSumVulnerabilityCounter.getSumVulnerabilities());
-               if (currFileSumVulnerabilityCounter.getHigh() != 0) {
-                   addMetric(context, file, SAST_HIGH_VULNERABILITIES, currFileSumVulnerabilityCounter.getHigh());
-               }
-               if (currFileSumVulnerabilityCounter.getMedium() != 0) {
-                   addMetric(context, file, SAST_MEDIUM_VULNERABILITIES, currFileSumVulnerabilityCounter.getMedium());
-               }
-               if (currFileSumVulnerabilityCounter.getLow() != 0) {
-                   addMetric(context, file, SAST_LOW_VULNERABILITIES, currFileSumVulnerabilityCounter.getLow());
-               }
-           }
+        if (rule == null) {
+            logger.info("Rule: " + "checkmarx_" + query.getId() + " is not active or not existing. It will not appear in Checkmarx scan results.");
+            logger.info("If rule exists in " + language.getSonarRuleRepository() + " rule repository, you can update it to your quality profile.");
         }
 
-        private void addNewVulnerabilitiesMetrics(SensorContext context, InputFile file){
-            if(currFileNewVulnerabilityCounter.getSumVulnerabilities() != 0 ) {
-                addMetric(context, file, SAST_TOTAL_NEW_VULNERABILITIES, currFileNewVulnerabilityCounter.getSumVulnerabilities());
-                if(currFileNewVulnerabilityCounter.getHigh() != 0) {
-                    addMetric(context, file, SAST_NEW_HIGH_VULNERABILITIES, currFileNewVulnerabilityCounter.getHigh());
+        return rule;
+    }
+
+    private void updateQueryToCurrFile(CxResultToSonarResult nonIssueResult) {
+        SastSeverity severity = SastSeverity.fromName(nonIssueResult.getResultData().getSeverity());
+        if (severity == null) {
+            logger.error("Result for query " + nonIssueResult.getQuery().getName() + " has no severity. Checkmarx result may be incomplete.");
+            return;
+        }
+        switch (severity) {
+            case SAST_HIGH:
+                currFileQueriesCollector.addHighQuery(nonIssueResult.getQuery().getName());
+                break;
+            case SAST_MEDIUM:
+                currFileQueriesCollector.addMediumQuery(nonIssueResult.getQuery().getName());
+                break;
+            case SAST_LOW:
+                currFileQueriesCollector.addLowQuery(nonIssueResult.getQuery().getName());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void updateCurrFileVulnerabilities(CxResultToSonarResult result) {
+        SastSeverity severity = SastSeverity.fromName(result.getResultData().getSeverity());
+        if (severity == null) {
+            logger.error("Result for query " + result.getQuery().getName() + " has no severity. Checkmarx result may be incomplete.");
+            return;
+        }
+        boolean isNew = "New".equals(result.getResultData().getStatus());
+
+        switch (severity) {
+            case SAST_HIGH:
+                currFileSumVulnerabilityCounter.incrementHigh();
+                if (isNew) {
+                    currFileNewVulnerabilityCounter.incrementHigh();
                 }
-                if(currFileNewVulnerabilityCounter.getMedium() != 0) {
-                    addMetric(context, file, SAST_NEW_MEDIUM_VULNERABILITIES, currFileNewVulnerabilityCounter.getMedium());
+                break;
+            case SAST_MEDIUM:
+                currFileSumVulnerabilityCounter.incrementMedium();
+                if (isNew) {
+                    currFileNewVulnerabilityCounter.incrementMedium();
                 }
-                if(currFileNewVulnerabilityCounter.getLow() != 0) {
-                    addMetric(context, file, SAST_NEW_LOW_VULNERABILITIES, currFileNewVulnerabilityCounter.getLow());
+                break;
+            case SAST_LOW:
+                currFileSumVulnerabilityCounter.incrementLow();
+                if (isNew) {
+                    currFileNewVulnerabilityCounter.incrementLow();
                 }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void saveCxCustomMetrics(SensorContext context, InputFile file) {
+        try {
+            addSumVulnerabilitiesMetrics(context, file);
+            addNewVulnerabilitiesMetrics(context, file);
+        } catch (Exception e) {
+            String errMsg = "Error saving Checkmarx vulnerabilities to to file: " + file.absolutePath() + "\nError: " + e.getMessage() +
+                    "\nPresented scan measures and report might be incomplete.";
+            logger.error(errMsg);
+            context.newAnalysisError().onFile(file).message(errMsg);
+        }
+    }
+
+    private void addSumVulnerabilitiesMetrics(SensorContext context, InputFile file) {
+        if (currFileSumVulnerabilityCounter.getSumVulnerabilities() > 0) {
+            addMetric(context, file, SAST_TOTAL_VULNERABILITIES, currFileSumVulnerabilityCounter.getSumVulnerabilities());
+            if (currFileSumVulnerabilityCounter.getHigh() != 0) {
+                addMetric(context, file, SAST_HIGH_VULNERABILITIES, currFileSumVulnerabilityCounter.getHigh());
+            }
+            if (currFileSumVulnerabilityCounter.getMedium() != 0) {
+                addMetric(context, file, SAST_MEDIUM_VULNERABILITIES, currFileSumVulnerabilityCounter.getMedium());
+            }
+            if (currFileSumVulnerabilityCounter.getLow() != 0) {
+                addMetric(context, file, SAST_LOW_VULNERABILITIES, currFileSumVulnerabilityCounter.getLow());
             }
         }
+    }
 
-        private void addMetric(final SensorContext context, final InputFile inputFile, final Metric<Integer> metric, int value) {
-            context.<Integer> newMeasure().forMetric(metric).on(inputFile).withValue(value).save();
-        }
-
-        private void saveCxQueriesMeasure(SensorContext context, InputFile file){
-            FileQueries fileQueries = currFileQueriesCollector.getAsFileQueriesObject();
-            String json = "";
-            try {
-                json = mapper.writeValueAsString(fileQueries);
-            }catch (JsonProcessingException e) {
-                logger.error("Error parsing Checkmarx queries du to exception: "+ e.getMessage());
-                logger.error("Details report may be incomplete.");
+    private void addNewVulnerabilitiesMetrics(SensorContext context, InputFile file) {
+        if (currFileNewVulnerabilityCounter.getSumVulnerabilities() != 0) {
+            addMetric(context, file, SAST_TOTAL_NEW_VULNERABILITIES, currFileNewVulnerabilityCounter.getSumVulnerabilities());
+            if (currFileNewVulnerabilityCounter.getHigh() != 0) {
+                addMetric(context, file, SAST_NEW_HIGH_VULNERABILITIES, currFileNewVulnerabilityCounter.getHigh());
             }
-            context.<String> newMeasure().forMetric(SAST_SCAN_QUERIES).on(file).withValue(json).save();
+            if (currFileNewVulnerabilityCounter.getMedium() != 0) {
+                addMetric(context, file, SAST_NEW_MEDIUM_VULNERABILITIES, currFileNewVulnerabilityCounter.getMedium());
+            }
+            if (currFileNewVulnerabilityCounter.getLow() != 0) {
+                addMetric(context, file, SAST_NEW_LOW_VULNERABILITIES, currFileNewVulnerabilityCounter.getLow());
+            }
         }
+    }
+
+    private void addMetric(final SensorContext context, final InputFile inputFile, final Metric<Integer> metric, int value) {
+        context.<Integer>newMeasure().forMetric(metric).on(inputFile).withValue(value).save();
+        logger.info("[CHECKMARX] Added measure, metric: " + metric.getName() + ", File: " + ((DefaultInputFile) inputFile).getProjectRelativePath());
+    }
+
+    private void saveCxQueriesMeasure(SensorContext context, InputFile file) {
+        FileQueries fileQueries = currFileQueriesCollector.getAsFileQueriesObject();
+        String json = "";
+        try {
+            json = mapper.writeValueAsString(fileQueries);
+        } catch (JsonProcessingException e) {
+            logger.error("Error parsing Checkmarx queries du to exception: " + e.getMessage());
+            logger.error("Details report may be incomplete.");
+        }
+        context.<String>newMeasure().forMetric(SAST_SCAN_QUERIES).on(file).withValue(json).save();
+        logger.info("[CHECKMARX] Added query measure, metric: " + SAST_SCAN_QUERIES.getName() + ", File: " + ((DefaultInputFile) file).getProjectRelativePath());
+    }
 }
